ebabe7ac611c3b405954b34d4fc6ee90fff11de9
LoneisCode <kjislone@gmail.com>
2022-07-25 22:29:22 -0500

    added a late night addition to image using Java script feature: can now click on img and goes straight to source

---
+++ Script/siteFunc.js
@@ -0,0 +1,8 @@
+const trumpetSrc = document.getElementById("splash-intro");
+
+trumpetSrc.addEventListener("click", (event)=>{
+    window.open("https://www.pexels.com/photo/bronze-statue-of-louis-prima-11948805/", "_blank");
+},false)
+
+
+

ee841b68d69c6f884dd9b9a5dbfb307227b6a575
LoneisCode <kjislone@gmail.com>
2022-08-12 15:38:31 -0500

    Finished the professional page and added a few functions to Js playing with the DOM

--- Script/siteFunc.js
+++ Script/siteFunc.js
@@ -1,8 +1,55 @@
-const trumpetSrc = document.getElementById("splash-intro");
+const trumpetSrc = document.getElementById("trumpet-pic");
 
 trumpetSrc.addEventListener("click", (event)=>{
-    window.open("https://www.pexels.com/photo/bronze-statue-of-louis-prima-11948805/", "_blank");
-},false)
+    window.open("https://www.pexels.com/photo/bronze-statue-of-louis-prima-11948805/", "_blank", "noreferrer noopener");
+},false);
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(sec =>{
+    sec.addEventListener("mouseover", (event)=>{
+        sec.style.textDecoration = "underline";
+    })
+    sec.addEventListener("mouseout",(e)=>{
+        sec.style.textDecoration ="none";
+    })
+})
+const toTop = document.getElementById("toTop");
+const logo = document.getElementById("logo");
+toTop.addEventListener("click", (e)=>{ logo.scrollIntoView({behavior: 'smooth'});});
+
+
+
+const resumeLink = document.querySelector("#res-link");
+resumeLink.addEventListener("click", (event)=>{
+    window.open("../Page/Images/Kniledge Johns - Resume(SWD).pdf","_blank","noopener noreferrer");
+},false);
+
+resumeLink.addEventListener("mouseover", (e)=>{resumeLink.style.cursor = "pointer"});
+
+const bulletLinks = document.querySelectorAll(".res-bull li a");
+const bulletLinksVisited = document.querySelectorAll(".res-bull li a:visited");
+
+
+ bulletLinks.forEach((link)=>{
+
+    link.addEventListener("mouseover",(e)=>{
+        link.style.fontSize = "1.5em"
+        link.style.color ="#FFBF00";
+    })
+    link.addEventListener("mouseout",(e)=>{
+        link.style.fontSize = "1em";
+        link.style.color ="black";
+    })
+    link.style.textDecoration ="none";
+    link.style.color ="black";
+});
+
+bulletLinksVisited.forEach((link)=>{  
+    link.style.textDecoration = "none";
+})
+
+
 
 
 
